fb87dd630feb205f13c24d54d7cad3575b02a5fb
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-14 10:31:57 +1300

    Read files and write total to file

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿var currentDirectory = Directory.GetCurrentDirectory();
+﻿using Newtonsoft.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
 Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -24,3 +29,25 @@ IEnumerable<string> FindFiles(string folderName)
 
   return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+  double salesTotal = 0;
+
+  // Loop over each file path in salesFiles
+  foreach (var file in salesFiles)
+  {
+    // Read the contents of the file
+    string salesJson = File.ReadAllText(file);
+
+    // Parse the contents as JSON
+    SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+    // Add the amount found in the Total field to the salesTotal variable
+    salesTotal += data?.Total ?? 0;
+  }
+
+  return salesTotal;
+}
+
+record SalesData(double Total);
